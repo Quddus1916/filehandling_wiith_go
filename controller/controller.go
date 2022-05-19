09d82464d430ec7f773ee875e28d8953cdf08015
Quddus1916 <quddusjunior1916@gmail.com>
2022-05-19 05:19:17 -0500

    file upload successful to a local directory

--- controller/controller.go
+++ controller/controller.go
@@ -1,13 +1,56 @@
 package controller
 
 import (
+	"example.com/database"
+	"path"
+	//"example.com/models"
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/mongo"
+	"io"
 	"net/http"
+	"os"
 )
 
+type customcontext struct {
+	echo.Context
+}
+
+var filecollection *mongo.Collection = database.OpenCollection(database.CLient, "userfiles")
+
 func Sendfile(c echo.Context) error {
-	msg := "send file working"
-	return c.JSON(http.StatusOK, msg)
+
+	//var userdata = new(models.User)
+	form, err := c.MultipartForm()
+
+	if err != nil {
+		//msg := "binding failed"
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	files := form.File["image"]
+
+	for _, file := range files {
+		// Source
+		src, err := file.Open()
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, file.Header)
+		}
+		defer src.Close()
+
+		uploadedfilename := file.Filename
+		uploadedfilepath := path.Join("./images", uploadedfilename)
+		dst, err := os.Create(uploadedfilepath)
+		if err != nil {
+
+			return c.JSON(http.StatusBadRequest, err.Error())
+		}
+		defer dst.Close()
+		if _, err = io.Copy(dst, src); err != nil {
+			return err
+		}
+
+	}
+	return c.JSON(http.StatusOK, files)
 }
 
 func Getfiles(c echo.Context) error {
