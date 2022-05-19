fff27165052ebd75193ae43b0d41fc3c6da1b8a1
Quddus1916 <quddusjunior1916@gmail.com>
2022-05-19 05:17:53 -0500

    database created

--- database/database.go
+++ database/database.go
@@ -1,1 +1,34 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb://localhost:27017"
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		panic(err.Error())
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	defer cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+}
+
+var CLient *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("FILE").Collection(collectionName)
+
+	return collection
+}
